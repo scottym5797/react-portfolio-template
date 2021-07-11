d4cc850bffff14776d058309c59346b8ed5690b0
scottym5797 <62399837+scottym5797@users.noreply.github.com>
2021-07-12 00:45:26 +0200

    added everything

--- src/components/about.js
+++ src/components/about.js
@@ -22,7 +22,7 @@ const About = () => {
             </p>
           </div>
           <div className="image-wrapper">
-            <img src={data.aboutImage} alt="about"></img>
+            <img className="image-dimensions" src={data.aboutImage} alt="about"></img>
           </div>
         </div>
       </div>
--- src/yourdata.js
+++ src/yourdata.js
@@ -1,102 +1,107 @@
 // Skills Icons
-import htmlIcon from "./images/html.svg"
-import cssIcon from "./images/css.svg"
-import reactIcon from "./images/react.svg"
-import jsIcon from "./images/javascript.svg"
-import designIcon from "./images/design.svg"
+import htmlIcon from "./images/Group.svg"
+import awsIcon from "./images/awsLogo.svg"
+import reactIcon from "./images/React-icon.svg"
+import jsIcon from "./images/JavaScript_logo_2.svg"
+import pythonIcon from "./images/python-logo.svg"
 import codeIcon from "./images/code.svg"
 
 // Social Icon
 import githubIcon from "./images/github.svg"
 import codepenIcon from "./images/codepen.svg"
 import dribbbleIcon from "./images/dribbble.svg"
-import instagramIcon from "./images/instagram.svg"
+import linkedinIcon from "./images/linkedin.svg"
+import halisi from "./images/halisi.png"
+import facebook from "./images/facebook_ads.png"
+import mixAmigo from "./images/MixAmigo.png"
+import me from "./images/me-resized-3.jpg"
 
 export default {
   //(Please Do Not Remove The comma(,) after every variable)
   //Change The Website Template
 
   //   Header Details ---------------------
-  name: "chetan",
+  name: "Scott",
   headerTagline: [
     //Line 1 For Header
-    "Building digital",
+    "Combining technical",
     //Line 2 For Header
-    "products, brands,",
+    "passion and expertise",
     //Line 3 For Header
-    "and experience",
+    "with domain knowledge",
   ],
   //   Header Paragraph
   headerParagraph:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
+    "Software Engineer with over 2 years experience using Python to build products used in automation and marketing.",
 
   //Contact Email
-  contactEmail: "hello@chetanverma.com",
+  contactEmail: "scott.martin5797@gmail.com",
 
   // End Header Details -----------------------
 
   // Work Section ------------------------
   projects: [
     {
-      title: "Project One", //Project Title - Add Your Project Title Here
+      title: "MixAmigo", //Project Title - Add Your Project Title Here
       para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+        "A multipage static website for getting song statistics for millions of songs in multiple langauges. Made in React.", // Add Your Service Type Here
       //Project Image - Add Your Project Image Here
       imageSrc:
-        "https://images.unsplash.com/photo-1487837647815-bbc1f30cd0d2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Njl8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+        mixAmigo,
       //Project URL - Add Your Project Url Here
-      url: "http://chetanverma.com/",
+      url: "https://mixamigo.com/",
     },
     {
-      title: "Project Two", //Project Title - Add Your Project Title Here
+      title: "Hali Halisi", //Project Title - Add Your Project Title Here
       para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+        "Progressive Web App to connect the Deaf to interpreters", // Add Your Service Type Here
       //Project Image - Add Your Project Image Here
       imageSrc:
-        "https://images.unsplash.com/photo-1605153864431-a2795a1b2f95?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwyNTY3ODl8fGVufDB8fHw%3D&auto=format&fit=crop&w=400&q=60",
+        halisi,
       //Project URL - Add Your Project Url Here
-      url: "http://chetanverma.com/",
+      url: "https://halisi.io/",
     },
     {
-      title: "Project Three", //Project Title - Add Your Project Title Here
+      title: "Facebook dynamic video creation", //Project Title - Add Your Project Title Here
       para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+        "Automated dynamic video creation with connection to FB Ads API for campaign creation and management. Using Python, OpenCV, Docker, React.js", // Add Your Service Type Here
       //Project Image - Add Your Project Image Here
       imageSrc:
-        "https://images.unsplash.com/photo-1504083898675-c896ecdae86e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjJ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+      facebook,
       //Project URL - Add Your Project Url Here
-      url: "http://chetanverma.com/",
+      url: "http://scottmartin.tech/",
     },
     {
-      title: "Project Four", //Project Title - Add Your Project Title Here
+      title: "SEA Campaign Automation", //Project Title - Add Your Project Title Here
       para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+        "Python based pipeline that creates advertisements for millions of keywords for 40 000+ products through Google Ads API.", // Add Your Service Type Here
       //Project Image - Add Your Project Image Here
       imageSrc:
-        "https://images.unsplash.com/photo-1492171983775-a51717616c0d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjB8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+        "https://images.unsplash.com/photo-1561451212-d7350aa8d04c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2690&q=80",
       //Project URL - Add Your Project Url Here
-      url: "http://chetanverma.com/",
+      url: "http://scottmartin.tech/",
     },
     {
-      title: "Project Five", //Project Title - Add Your Project Title Here
+      title: "Corona Tracker", //Project Title - Add Your Project Title Here
       para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+        "Tool to check the current number of corona infections", // Add Your Service Type Here
       //Project Image - Add Your Project Image Here
       imageSrc:
-        "https://images.unsplash.com/photo-1534239143101-1b1c627395c5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NzZ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+        "https://images.unsplash.com/photo-1583324113626-70df0f4deaab?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2689&q=80",
       //Project URL - Add Your Project Url Here
-      url: "http://chetanverma.com/",
+      url: "https://stupefied-yonath-e6b6a3.netlify.app/",
     },
     {
-      title: "Project Six", //Project Title - Add Your Project Title Here
+      title: "Corona Vaccine Scraper", //Project Title - Add Your Project Title Here
       para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+        "Scraper that checks for appointments in Munich every minute and sends an email when available (private repo: ask for access)", // Add Your Service Type Here
       //Project Image - Add Your Project Image Here
       imageSrc:
-        "https://images.unsplash.com/photo-1517479149777-5f3b1511d5ad?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTAyfHxwYXN0ZWx8ZW58MHx8MHw%3D&auto=format&fit=crop&w=400&q=60",
+        "https://images.unsplash.com/photo-1618961734760-466979ce35b0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2689&q=80",
       //Project URL - Add Your Project Url Here
-      url: "http://chetanverma.com/",
+      url: "http://scottmartin.tech/",
     },
+
 
     /*
     If You Want To Add More Project just Copy and Paste This At The End
@@ -113,13 +118,13 @@ export default {
 
   // About Secton --------------
   aboutParaOne:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    "Versatile and motivated professional working in the marketing tech field. My passion and vision is to utilise the rising ubiquity of data and need for digitalisation to make a significant impact on how we operate, this is my primary focus at Hali Halisi and at FlixBus. In Australia, I underwent 3 years of officer training in the Australian Defence Force. I can communicate effectively and lead in dynamic and adverse situations, proven within my formal recommendation letter from the US Military after spending a semester on exchange at Purdue University.",
   aboutParaTwo:
-    "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    "I've worked for over 2 years as a Python/Data Engineer at FlixBus in the marketing space. My focus was on automation of tasks and the creation of data pipelines to facilitate products.",
   aboutParaThree:
-    "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    "In my spare time I create other products, particularly web apps. Such as MixAmigo and the MVP for Hali Halisi. As such I have full stack skills with an expertise in Python and AWS.",
   aboutImage:
-    "https://images.unsplash.com/photo-1519713958759-6254243c4a53?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+    me,
 
   //   End About Section ---------------------
 
@@ -129,34 +134,35 @@ export default {
 
   skills: [
     {
-      img: htmlIcon,
+      img: pythonIcon,
       para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+        "Strong experience in Python developing ETL pipelines and backend products.",
     },
     {
-      img: cssIcon,
+      img: awsIcon,
       para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
-    },
-    {
-      img: jsIcon,
-      para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
-    },
-    {
-      img: reactIcon,
-      para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
-    },
-    {
-      img: designIcon,
-      para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+        "Expereience with deploying and using Amazon services such as Lambda, S3, EC2, RDS",
     },
     {
       img: codeIcon,
       para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+        "Development mindset including skills in Agile, Jira, Git, Docker, Kubernetes, creating APIs, etc",
+    },
+    {
+      img: jsIcon,
+      para:
+        "JavaScript in both web development and NodeJS for backend development or scraping.",
+    },
+    {
+      img: reactIcon,
+      para:
+        "React framework for the creation of websites or Progressive Web Apps.",
+    },
+
+    {
+      img: htmlIcon,
+      para:
+        "Snowflake, Postgres, SQL and general database management.",
     },
   ],
 
@@ -164,28 +170,20 @@ export default {
 
   //   Promotion Section --------------------------
 
-  promotionHeading: "Heading",
-  promotionPara:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  // promotionHeading: "Heading",
+  // promotionPara:
+  //   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
   // End Promotion Section -----------------
 
   //   Contact Section --------------
 
-  contactSubHeading: "Let's create your next experience together",
+  contactSubHeading: "I look forward to hearing from you",
   social: [
     // Add Or Remove The Link Accordingly
-    { img: githubIcon, url: "https://github.com/chetanverma16" },
+    { img: githubIcon, url: "https://github.com/scottym5797" },
     {
-      img: codepenIcon,
-      url: "https://www.codepen.com/",
-    },
-    {
-      img: dribbbleIcon,
-      url: "https://dribbble.com/chetanverma",
-    },
-    {
-      img: instagramIcon,
-      url: "https://www.instagram.com/",
+      img: linkedinIcon,
+      url: "https://www.linkedin.com/in/scott-martin-43a639118/",
     },
   ],
 
