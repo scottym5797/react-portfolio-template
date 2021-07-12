cd3c907539ff82b2e44038ddcd75eb678575ee17
scottym5797 <62399837+scottym5797@users.noreply.github.com>
2021-07-12 13:17:50 +0200

    Update facebook.js

--- src/pages/project/facebook.js
+++ src/pages/project/facebook.js
@@ -13,9 +13,9 @@ const WorkPage = () => (
     <Header></Header>
     <About 
     title={"Dynamic Video Creation"}
-    paraOne={"A long running product developing both the system to create videos dynamically through connection to a product database and also upload and manage advertisements using these videos through Facebook Ads API."} 
+    paraOne={"A long running product, developing both the system to create videos dynamically through connection to a product database, and also upload/manage the advertisements using these videos through Facebook Ads API."} 
     paraTwo={"Python, Postgres, OpenCV, Docker, Flask API"} 
-    paraThree={"The product successfully runs dynamic ads in 100+ cities. As well as updates the dynamic prices and products daily."} 
+    paraThree={"The product successfully creates videos, and runs dynamic ads in 100+ cities. As well as updates the dynamic prices and products daily."} 
     imgUrl={"https://images.unsplash.com/photo-1563986768494-4dee2763ff3f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=80"}
     projectLink={`mailto:scott.martin5797@gmail.com`}
     urlText={"Contact for more info"}></About>
