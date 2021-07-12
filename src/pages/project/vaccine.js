1dd6096e9759d53de5185f17546e44cd237448af
scottym5797 <62399837+scottym5797@users.noreply.github.com>
2021-07-12 13:23:06 +0200

    Update vaccine.js

--- src/pages/project/vaccine.js
+++ src/pages/project/vaccine.js
@@ -13,9 +13,9 @@ const WorkPage = () => (
     <Header></Header>
     <About 
     title={"Vaccine Scraper"}
-    paraOne={"In April/May 2021 Germany allowed all individuals to access AstraZeneca Vaccine. Due to the high demand appoitments were being booked and cancelled very often. As such there needed to be a way to programmatically check for availabilities and notify those who were interested."} 
+    paraOne={"In April/May 2021 Germany allowed all individuals to access AstraZeneca Vaccine. Due to the high demand appointments were very dynamic; being booked or cancelled often. As such there needed to be a way to programmatically check for availabilities and notify those who were interested."} 
     paraTwo={"Python, Selenium"} 
-    paraThree={"In an afternoon I developed a script that checked the internet (every minute) at a number of sources for available vaccine appointments in Munich and notified via email when they were available. I successfully booked appointements for myself and roughly 12 other work colleagoues within the following days."} 
+    paraThree={"In an afternoon I developed a script that checked the internet (every minute) at a number of sources for available vaccine appointments in Munich and notified via email when they were available. I successfully booked appointments for myself and roughly 12 other work colleagues within the following days."} 
     imgUrl={"https://images.unsplash.com/photo-1618961734760-466979ce35b0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=80"}
     projectLink={`mailto:scott.martin5797@gmail.com`}
     urlText={"Contact for access"}></About>
